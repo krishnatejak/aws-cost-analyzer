9237c5f0554b59ee95ec2b63d1e45ba283893b45
krishnatejak <kkt.iitm@gmail.com>
2025-01-02 21:35:17 +0530

    Remove gunicorn and fix imports

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -1,4 +1,6 @@
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from app import app
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0', port=5000)
